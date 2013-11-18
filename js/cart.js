8f42eaf55b902b50db1fb2e0f6d00c742ec306ff
Bryan Gula <gulabry@uw.edu>
2013-11-17 20:47:04 -0800

    Lab 8 Final

--- js/cart.js
+++ js/cart.js
@@ -9,5 +9,21 @@
 */
 
 function createCartModel(config) {
+        var model = createListModel(config);
+        
+        model.getTotalPrice = function() {
+                var idx;
+                var totalPrice = 0;
+                for(idx = 0; idx < this.items.length; ++idx){
+                        totalPrice += this.items[idx].price;
+                }
+                return totalPrice.toFixed(2);
+        }; //getTotalPrice
 
+        model.toJSON = function() {
+                var modelString = JSON.stringify(this.model);
+                return modelString;
+        }; //toJSON
+
+        return model;
 } //createCartModel()
